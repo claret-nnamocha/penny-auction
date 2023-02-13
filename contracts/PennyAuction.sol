c65d270156a95d133fdade7f4ca170433ac7d0b8
claretnnamocha <devclareo@gmail.com>
2023-02-13 21:18:54 +0100

    Update PennyAuction.sol

--- contracts/PennyAuction.sol
+++ contracts/PennyAuction.sol
@@ -5,29 +5,30 @@ import "./AuctionItem.sol";
 
 contract PennyAuction {
     event NewHighestBid(address bidder, uint256 bid, uint256 itemId);
-    event AuctionStarted(uint256 startingBid, uint256 itemId);
+    event AuctionStarted(uint256 itemId);
     event AuctionEnded(address winner, uint256 highestBid, uint256 itemId);
 
     AuctionItem public auctionItem;
     mapping(uint256 => AuctionItem) public auctionItems;
 
-    mapping(uint256 => uint256) public highestBids;
+    mapping(uint256 => uint256) public currentPrices;
+    mapping(uint256 => uint256) public highestBidCounts;
+    mapping(uint256 => mapping(address => uint256)) public bids;
     mapping(uint256 => address) public highestBidders;
+    mapping(uint256 => bool) public activeBids;
     mapping(uint256 => uint256) public biddingDeadlines;
-    mapping(uint256 => uint256) public startingBids;
-    mapping(uint256 => uint256) public increasingBidSizes;
+    mapping(uint256 => uint256) public biddingFees;
 
     address public owner;
     uint256 public productCount = 0;
 
-    function addProduct(AuctionItem _auctionItem, uint256 _startingBid) public {
+    function addProduct(AuctionItem _auctionItem) public {
         require(
             msg.sender == owner,
             "Only the auction owner can add a product."
         );
 
         auctionItems[++productCount] = _auctionItem;
-        startingBids[productCount] = _startingBid;
     }
 
     function startAuction(uint256 itemId) public {
@@ -36,22 +37,23 @@ contract PennyAuction {
             "Only the auction owner can start an auction."
         );
         require(
-            biddingDeadlines[itemId] == 0,
+            !activeBids[itemId],
             "An auction for this product is already in progress."
         );
-        uint256 biddingDeadline = block.timestamp + 60;
-        biddingDeadlines[itemId] = biddingDeadline;
 
-        highestBids[itemId] = startingBids[itemId];
+        biddingDeadlines[itemId] = block.timestamp + 20;
+        currentPrices[itemId] = 0;
+        highestBidCounts[itemId] = 0;
+        activeBids[itemId] = true;
 
-        emit AuctionStarted(startingBids[itemId], itemId);
+        emit AuctionStarted(itemId);
     }
 
     function bid(uint256 itemId) public payable {
         require(msg.sender != owner, "The auction owner cannot place a bid.");
 
         require(
-            msg.value == increasingBidSizes[itemId],
+            msg.value == biddingFees[itemId],
             "The bidding amount is incorrect"
         );
 
@@ -60,12 +62,19 @@ contract PennyAuction {
             "The bidding deadline has passed."
         );
 
-        uint256 _bid = highestBids[itemId] + increasingBidSizes[itemId];
+        require(activeBids[itemId], "This item is not up for bidding.");
+
+        uint256 _bid = currentPrices[itemId] + biddingFees[itemId];
 
         biddingDeadlines[itemId] = biddingDeadlines[itemId] + 20;
 
-        highestBids[itemId] = _bid;
-        highestBidders[itemId] = msg.sender;
+        currentPrices[itemId] = _bid;
+        bids[itemId][msg.sender] += 1;
+
+        if (highestBidCounts[itemId] < bids[itemId][msg.sender]) {
+            highestBidCounts[itemId] = bids[itemId][msg.sender];
+            highestBidders[itemId] = msg.sender;
+        }
 
         emit NewHighestBid(msg.sender, _bid, itemId);
     }
@@ -80,11 +89,33 @@ contract PennyAuction {
             "The bidding deadline has not passed."
         );
 
-        auctionItems[itemId].transferOwnership(highestBidders[itemId]);
-
-        emit AuctionEnded(highestBidders[itemId], highestBids[itemId], itemId);
+        emit AuctionEnded(
+            highestBidders[itemId],
+            currentPrices[itemId],
+            itemId
+        );
 
         biddingDeadlines[itemId] = 0;
+        activeBids[itemId] = false;
+    }
+
+    function claim(uint256 itemId) public payable {
+        require(
+            !activeBids[itemId] || biddingDeadlines[itemId] == 0,
+            "This item is still up for bidding."
+        );
+
+        require(
+            msg.sender == highestBidders[itemId],
+            "You are not the winner."
+        );
+
+        require(
+            msg.value == currentPrices[itemId],
+            "The item amount is incorrect"
+        );
+
+        auctionItems[itemId].transferOwnership(highestBidders[itemId]);
         highestBidders[itemId] = address(0);
     }
 }
